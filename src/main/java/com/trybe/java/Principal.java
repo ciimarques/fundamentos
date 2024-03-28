5e6958b037a768c9a336b7e4c7820616b81afcb9
Cintia Marques <cintiamarques.mk@gmail.com>
2024-03-28 18:23:39 -0300

    Req 2

---
+++ src/main/java/com/trybe/java/Principal.java
@@ -0,0 +1,7 @@
+package com.trybe.java;
+
+public class Principal {
+    public static void main(String[] args) {
+        Residente residente = new Residente("João", 22, 72, 1.75);
+    }
+}
--- src/main/java/com/trybe/java/Residente.java
+++ src/main/java/com/trybe/java/Residente.java
@@ -4,5 +4,20 @@ package com.trybe.java;
  * Classe.
  **/
 public class Residente {
-
+    public String nome;
+    public int idade;
+    public double peso;
+    public double altura;
+    public Residente(String nome, int idade, double peso, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+        this.altura = altura;
+    }
+    public void imprimeDados() {
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Idade: " + this.idade);
+        System.out.println("Peso: " + this.peso);
+        System.out.println("Altura: " + this.altura);
+    }
 }
